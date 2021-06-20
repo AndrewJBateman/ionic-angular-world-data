dce7333c2b15a5b60d4319ac55008f6d7f6e9f57
AndrewJBateman <gomezbateman@yahoo.com>
2021-06-20 12:53:42 +0200

    fix: translate error fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ export function createTranslateLoader(http: HttpClient) {
 		HttpClientModule,
 		IonicStorageModule.forRoot(),
 		TranslateModule.forRoot({
+      defaultLanguage:'en',
 			loader: {
 				provide: TranslateLoader,
 				useFactory: createTranslateLoader,
