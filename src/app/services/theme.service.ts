569af463f8e50091cb8dbc3431499d61403994f5
AndrewJBateman <gomezbateman@yahoo.com>
2024-01-28 07:41:45 +0100

    refactor: use await for storage access, add class dark-theme, commenting

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,3 +1,8 @@
+/**
+ * ThemeService class responsible for managing the theme mode of the application.
+ * It provides methods to enable dark mode, enable light mode, and change the theme mode based on user interaction.
+ * The class uses the Renderer2 and Storage modules from Angular to manipulate the DOM and store the theme mode.
+ */
 import { Injectable, RendererFactory2, Inject, Renderer2 } from "@angular/core";
 
 import { DOCUMENT } from "@angular/common";
@@ -18,15 +23,17 @@ export class ThemeService {
     this.renderer = this.rendererFactory.createRenderer(null, null);
   }
 
+  THEME_CLASS = "dark-theme";
+  
   async enableDark(): Promise<void> {
     this.renderer.addClass(this.document.body, "dark-theme");
-		this.storage.set("dark-theme", true);
+		await this.storage.set(this.THEME_CLASS, true);
     this.darkMode = true;
   }
 
   async enableLight(): Promise<void> {
     this.renderer.removeClass(this.document.body, "dark-theme");
-    this.storage.set("dark-theme", false);
+    await this.storage.set(this.THEME_CLASS, false);
     this.darkMode = false;
   }
 
