9380417d7a1a5feb928ecf9c783706ede5373f38
AndrewJBateman <gomezbateman@yahoo.com>
2024-02-04 19:06:11 +0100

    refactor: about standalone compt, add try catch block, commenting, reduce code

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -6,20 +6,25 @@ import { PopoverController } from "@ionic/angular";
 import { PopoverPage } from "../about-popover/about-popover";
 
 @Component({
-	selector: "app-about",
-	templateUrl: "./about.page.html",
-	styleUrls: ["./about.page.scss"],
-	standalone: true,
-	imports: [IonicModule]
+  selector: "app-about",
+  templateUrl: "./about.page.html",
+  styleUrls: ["./about.page.scss"],
+  standalone: true,
+  imports: [IonicModule],
 })
 export class AboutPage {
-	constructor(public popoverCtrl: PopoverController) {}
+  constructor(private popoverCtrl: PopoverController) {}
 
-	async presentPopover(event: any) {
-		const popover = await this.popoverCtrl.create({
-			component: PopoverPage,
-			event,
-		});
-		await popover.present();
-	}
+  async presentPopover(event: Event): Promise<void> {
+    try {
+      const popover = await this.popoverCtrl.create({
+        component: PopoverPage,
+        event,
+      });
+      await popover.present();
+    } catch (err: unknown) {
+      console.error("Error presenting popover:", err);
+      // Handle the error or display an error message to the user
+    }
+  }
 }
